e1d509802e906d14d5ba60fb602d3641016dac8c
w <none>
2020-06-28 16:55:22 +0000

    add client register

--- client/index.js
+++ client/index.js
@@ -1,9 +1,37 @@
 const EventEmitter = require('events');
+const http = require('http');
 
 const WebSocket = require('ws');
 
-function register(alias, token, intents, criteria, dst, clientSecret) {
-	// todo
+function register({alias, token, intents, criteria, dst, clientSecret, requestModule, endpoint}) {
+	if (!alias) throw new Error('missing alias');
+	if (!token) throw new Error('missing token');
+	if (!intents) intents = 4608;
+	if (!criteria) criteria = {t: 'MESSAGE_CREATE'};
+	if (!dst) throw new Error('missing dst');
+	if (!clientSecret) throw new Error('missing clientSecret');
+	// todo: switch in public deployment
+	if (!requestModule) requestModule = http;
+	if (!endpoint) endpoint = 'http://localhost:3000';
+
+	// todo: add a way to rotate clientSecret
+	const body = Buffer.from(JSON.stringify({token, intents, criteria, dst, clientSecret}));
+	return new Promise((resolve, reject) => {
+		const req = requestModule.request(endpoint + '/relays/' + alias, {
+			auth: clientSecret,
+			headers: {
+				'Content-Type': 'application/json',
+				'Content-Length': body.byteLength,
+			},
+		}, (res) => {
+			if (res.statusCode < 400) {
+				resolve(res);
+			} else {
+				reject(res);
+			}
+		});
+		req.end(body);
+	});
 }
 
 class Client extends EventEmitter {
