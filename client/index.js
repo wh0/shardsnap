e661928977e482ff899cff4bc30ecb7d2e021440
w <none>
2020-06-28 06:04:12 +0000

    check in client lib and sample

---
+++ client/index.js
@@ -0,0 +1,26 @@
+const EventEmitter = require('events');
+
+const WebSocket = require('ws');
+
+function register(alias, token, intents, criteria, dst, clientSecret) {
+	// todo
+}
+
+class Client extends EventEmitter {
+	constructor(alias, clientSecret, wsOpts) {
+		super();
+		this.authLine = 'Basic ' + Buffer.from(clientSecret).toString('base64');
+		this.wss = new WebSocket.Server({path: '/dcc/v1/' + alias, ...wsOpts});
+		this.wss.on('connection', (socket, req) => {
+			if (req.headers.authorization !== this.authLine) {
+				socket.close(4001);
+				return;
+			}
+			socket.on('message', (data) => {
+				this.emit('packet', JSON.parse(data));
+			});
+		});
+	}
+}
+
+module.exports = {register, Client};
---
+++ sample-cookout/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const express = require('express');
+
+const dcc = require('../client');
+
+const app = express();
+app.get('/zero', (req, res) => {
+	res.json(0);
+});
+
+const server = http.createServer(app);
+
+const config = require('../config.json');
+const dccc = new dcc.Client(config.alias, config.clientSecret, {server});
+dccc.on('packet', (packet) => {
+	console.log('received packet', JSON.stringify(packet));
+});
+
+server.listen(process.env.PORT, () => {
+	console.log('listening');
+});
